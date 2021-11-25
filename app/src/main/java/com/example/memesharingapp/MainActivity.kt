361af70d7bcc97c85bcfd95deaf43cc6b9a464fd
Raghav <raghavarya2002@gmail.com>
2021-11-25 19:48:15 +0530

    Done Glide , and learnt a new thing like chooser to share something via different apps :(

--- app/src/main/java/com/example/memesharingapp/MainActivity.kt
+++ app/src/main/java/com/example/memesharingapp/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.memesharingapp
 
+import android.content.Intent
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Request
@@ -10,8 +13,14 @@ import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 
 class MainActivity : AppCompatActivity() {
+    var currentImageUrl: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -20,9 +29,10 @@ class MainActivity : AppCompatActivity() {
         loadmeme()
     }
 
-    private fun loadmeme(){
+    private fun loadmeme() {
         val imageView: ImageView = findViewById(R.id.memeImageView)
         val textView = findViewById<TextView>(R.id.text)
+        val progressBar: ProgressBar = findViewById(R.id.progressBar)
 
 // ...
 
@@ -35,8 +45,31 @@ class MainActivity : AppCompatActivity() {
             Request.Method.GET, url, null,
             { response ->
 
-                val url = response.getString("url")
-                Glide.with(this).load(url).into(imageView)
+                currentImageUrl = response.getString("url")
+                Glide.with(this).load(currentImageUrl).listener(object : RequestListener<Drawable> {
+
+                    override fun onLoadFailed(
+                        e: GlideException?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        progressBar.visibility = View.GONE
+                        return false
+                    }
+
+                    override fun onResourceReady(
+                        resource: Drawable?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        dataSource: DataSource?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        progressBar.visibility = View.GONE
+                        return false
+
+                    }
+                }).into(imageView)
 
 
             },
@@ -53,6 +86,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun shareMeme(view: View) {
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "text/plain"
+        intent.putExtra(Intent.EXTRA_TEXT, "Hey , Chcek this awesome meme $currentImageUrl")
+        val chooser = Intent.createChooser(intent, "Share this meme using..")
+        startActivity(chooser)
 
     }
 
