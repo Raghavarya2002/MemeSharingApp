8dd6e86ddacb441d5361cc636e23d8744a9a8a84
Raghav <raghavarya2002@gmail.com>
2021-11-25 19:25:34 +0530

    Done API Call

--- app/src/main/java/com/example/memesharingapp/MainActivity.kt
+++ app/src/main/java/com/example/memesharingapp/MainActivity.kt
@@ -1,45 +1,59 @@
 package com.example.memesharingapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Request
 import com.android.volley.Response
-import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
+import com.bumptech.glide.Glide
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        loadmeme()
     }
 
     private fun loadmeme(){
+        val imageView: ImageView = findViewById(R.id.memeImageView)
         val textView = findViewById<TextView>(R.id.text)
+
 // ...
 
 // Instantiate the RequestQueue.
         val queue = Volley.newRequestQueue(this)
-        val url = "https://www.google.com"
+        val url = "https://meme-api.herokuapp.com/gimme"
 
 // Request a string response from the provided URL.
-        val stringRequest = StringRequest(
-            Request.Method.GET, url,
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.GET, url, null,
             { response ->
-                Log.d("success request" , response.substring(0,500))
+
+                val url = response.getString("url")
+                Glide.with(this).load(url).into(imageView)
+
 
             },
-            Response.ErrorListener{
-                Log.d("error" , it.localizedMessage)
+            Response.ErrorListener {
+
             })
 
 // Add the request to the RequestQueue.
-        queue.add(stringRequest)
+        queue.add(jsonObjectRequest)
     }
 
-    fun showNextMeme(view: View) {}
-    fun shareMeme(view: View) {}
+    fun showNextMeme(view: View) {
+        loadmeme()
+    }
+
+    fun shareMeme(view: View) {
+
+    }
 
 }
